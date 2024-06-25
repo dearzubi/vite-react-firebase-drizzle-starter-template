6ea8f705b4264302768451e39e4968dac956b3b8
Zubair Khalid <zubairkhalidce@gmail.com>
2024-06-25 16:01:55 +0100

    feat: added drizzle ORM and postgresql database support

---
+++ backend/functions/drizzle.config.ts
@@ -0,0 +1,21 @@
+import * as dotenv from "dotenv";
+import {defineConfig} from "drizzle-kit";
+
+dotenv.config({
+  path: ".env.local",
+});
+
+export default defineConfig({
+  dialect: "postgresql", // "mysql" | "sqlite"
+  schema: "./src/db/schemas/*",
+  out: "./drizzle",
+  verbose: true,
+  strict: true,
+  breakpoints: false,
+  migrations: {
+    schema: "public", // used in PostgreSQL only and default to `drizzle`
+  },
+  dbCredentials: {
+    url: process.env.DB_URL!,
+  },
+});
---
+++ backend/functions/src/db/index.ts
@@ -0,0 +1,12 @@
+import {drizzle} from "drizzle-orm/postgres-js";
+import * as postgres from "postgres";
+// import * as authSchema from "./schemas/auth";
+
+const connectionString = process.env.DB_URL;
+
+if (!connectionString) {
+  throw new Error("DB_URL env variable is not set");
+}
+
+const client = postgres(connectionString);
+export const db = drizzle(client, {schema: {}});
